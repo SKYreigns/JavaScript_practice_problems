6bf5f6364246c3cc3eca6fae3b604a56ccb9f553
SUMAN <sumankumaryaduvanshi1@gmail.com>
2025-08-05 15:57:14 +0000

    added solution of intermediate problem 09.

---
+++ Array_problems/INTERMEDIATE_PROBLEMS/Array_I-P_09.js
@@ -0,0 +1,9 @@
+// <Problem_9: Sort Strings Alphabetically
+// <Task: Given an array of strings, sort them in alphabetical order. The original array should be modified.
+// <Example_Input: ['banana', 'apple', 'cherry', 'date']
+// <Expected_Output_(Modified_Array): ['apple', 'banana', 'cherry', 'date']>
+
+
+let fruits = ['banana', 'apple', 'cherry', 'date'];
+let sortedFruits = fruits.sort();
+console.log(sortedFruits);
