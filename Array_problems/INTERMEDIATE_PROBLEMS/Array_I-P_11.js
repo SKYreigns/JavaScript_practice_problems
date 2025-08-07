abac1c8e59cd282e5d38dc2849d8b3b24289b2ef
SUMAN <sumankumaryaduvanshi1@gmail.com>
2025-08-07 15:05:58 +0000

    Added solution of Array Intermediate Problem 11.

---
+++ Array_problems/INTERMEDIATE_PROBLEMS/Array_I-P_11.js
@@ -0,0 +1,10 @@
+
+// <Problem_11: Convert a Sentence String to an Array of Words
+// <Task: Given a sentence as a single string, convert it into a new array where each element is a word from the sentence. Words are separated by spaces.
+// <Example_Input: "Hello world from JavaScript"
+// <Expected_Output: ['Hello', 'world', 'from', 'JavaScript']>
+
+
+let Sentence = "Hello world from JavaScript";
+let myArray = Sentence.split(' ');
+console.log(myArray);
