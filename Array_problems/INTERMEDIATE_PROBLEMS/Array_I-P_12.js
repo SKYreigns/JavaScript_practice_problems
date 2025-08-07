ecf0630ef43ffdf1c0f5e716cc8c41fa42d4e7a2
SUMAN <sumankumaryaduvanshi1@gmail.com>
2025-08-07 15:16:34 +0000

    Added solution to Array Intermediate Problem 12.

---
+++ Array_problems/INTERMEDIATE_PROBLEMS/Array_I-P_12.js
@@ -0,0 +1,10 @@
+// <Problem_12: Convert an Array of Words to a Single Sentence String
+// <Task: Given an array of words (strings), combine them into a single string forming a sentence, with each word separated by a single space.
+// <Example_Input: ['This', 'is', 'a', 'test', 'sentence']
+// <Expected_Output: "This is a test sentence">
+
+let separatedArray = ['This', 'is', 'a', 'test', 'sentence'];
+
+let JoinedArray = separatedArray.join(' ');
+
+console.log(JoinedArray);
