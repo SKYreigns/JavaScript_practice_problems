078e2e46ec1bdd66829927938c9d88ecf9187d2e
SUMAN <sumankumaryaduvanshi1@gmail.com>
2025-08-07 14:46:50 +0000

    Added the solution of Intermediate array problem 10.

---
+++ Array_problems/INTERMEDIATE_PROBLEMS/Array_I-P_10.js
@@ -0,0 +1,11 @@
+// <Problem_10: Reverse the Order of Elements
+// <Task: Given an array of elements, reverse the order of its elements. The original array should be modified directly.
+// <Example_Input: [1, 2, 3, 4, 5]
+// <Expected_Output_(Modified_Array): [5, 4, 3, 2, 1]>
+
+let array = [1, 2, 3, 4, 5];
+console.log('Original Array: ', array);
+
+let reversedArray = array.reverse();
+
+console.log('Reversed Array: ', reversedArray);
